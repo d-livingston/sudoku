ba25fd30976cc00068f84fcded56d64b4cf76622
Daniel Livingston <me@daniellivingston.net>
2021-05-14 17:04:26 -0700

    Added async versions of network solve methods

--- src/network/solve.ts
+++ src/network/solve.ts
@@ -6,11 +6,58 @@ import { NetworkSolution, NetworkEventType } from "./types";
 
 declare module "./network" {
     interface Network {
-        solve(): NetworkSolution;
+        solve(): Promise<NetworkSolution>;
+        solveSync(): NetworkSolution;
     }
 }
 
-Network.prototype.solve = function (): NetworkSolution {
+Network.prototype.solve = async function (): Promise<NetworkSolution> {
+    const historyStartLength = this.networkHistory.length;
+    const solutionStartLength = this.currentSolutionState.length;
+
+    try {
+        let hasMultipleSolutions: boolean = false;
+        let solution: Node[] = [];
+        let nodesTried = 0;
+
+        const onSolutionFound = (sol: Node[]) => {
+            if (solution.length === 0) {
+                solution = [...sol];
+            } else {
+                hasMultipleSolutions = true;
+            }
+        };
+        const assertStopSolving = () => hasMultipleSolutions;
+        const onColumnChoice = () => nodesTried++;
+
+        await search(this, {
+            onSolutionFound,
+            assertStopSolving,
+            onColumnChoice,
+        });
+
+        return {
+            solution,
+            hasMultipleSolutions,
+            nodesTried,
+        };
+    } catch {
+        // Return the network to its initial state and return default values.
+        while (this.networkHistory.length > historyStartLength) {
+            await this.dispatch(NetworkEventType.Undo);
+        }
+        while (this.currentSolutionState.length > solutionStartLength) {
+            this.currentSolutionState.pop();
+        }
+        return {
+            solution: [],
+            hasMultipleSolutions: false,
+            nodesTried: 0,
+        };
+    }
+};
+
+Network.prototype.solveSync = function (): NetworkSolution {
     let hasMultipleSolutions: boolean = false;
     let solution: Node[] = [];
     let nodesTried = 0;
@@ -25,7 +72,7 @@ Network.prototype.solve = function (): NetworkSolution {
     const assertStopSolving = () => hasMultipleSolutions;
     const onColumnChoice = () => nodesTried++;
 
-    search(this, {
+    searchSync(this, {
         onSolutionFound,
         assertStopSolving,
         onColumnChoice,
@@ -44,7 +91,50 @@ type SearchOptions = {
     onColumnChoice: (c: Node) => void;
 };
 
-const search = (
+const search = async (
+    network: Network,
+    { assertStopSolving, onSolutionFound, onColumnChoice }: SearchOptions
+): Promise<void> => {
+    if (assertStopSolving()) return;
+    if (network.isEmpty()) {
+        // Current iteration is complete and a solution is found
+        onSolutionFound(network.currentSolutionState);
+    } else {
+        // Find the next smallest column and cover it.
+        // Try each node in the column as part of the solution.
+        const column = network.reduce<Node>((acc, c) => {
+            if (!acc) return c;
+            return c.size < acc.size ? c : acc;
+        });
+        if (!column || column.size === 0)
+            throw new Error("Internal network error.");
+        onColumnChoice(column);
+
+        await network.dispatch(NetworkEventType.Cover, column);
+        column.forEach("down", (r) => {
+            network.currentSolutionState.push(r);
+            r.forEach(
+                "right",
+                async (j) =>
+                    await network.dispatch(NetworkEventType.Cover, j.column)
+            );
+            searchSync(network, {
+                assertStopSolving,
+                onSolutionFound,
+
+                onColumnChoice,
+            });
+            r.forEach(
+                "left",
+                async () => await network.dispatch(NetworkEventType.Undo)
+            );
+            network.currentSolutionState.pop();
+        });
+        await network.dispatch(NetworkEventType.Undo);
+    }
+};
+
+const searchSync = (
     network: Network,
     { assertStopSolving, onSolutionFound, onColumnChoice }: SearchOptions
 ) => {
@@ -61,21 +151,23 @@ const search = (
         })!;
         onColumnChoice(column);
 
-        network.dispatch(NetworkEventType.Cover, column);
+        network.dispatchSync(NetworkEventType.Cover, column);
         column.forEach("down", (r) => {
             network.currentSolutionState.push(r);
             r.forEach("right", (j) =>
-                network.dispatch(NetworkEventType.Cover, j.column)
+                network.dispatchSync(NetworkEventType.Cover, j.column)
             );
-            search(network, {
+            searchSync(network, {
                 assertStopSolving,
                 onSolutionFound,
 
                 onColumnChoice,
             });
-            r.forEach("left", () => network.dispatch(NetworkEventType.Undo));
+            r.forEach("left", () =>
+                network.dispatchSync(NetworkEventType.Undo)
+            );
             network.currentSolutionState.pop();
         });
-        network.dispatch(NetworkEventType.Undo);
+        network.dispatchSync(NetworkEventType.Undo);
     }
 };
--- src/tests/network/solve.test.ts
+++ src/tests/network/solve.test.ts
@@ -1,27 +1,27 @@
 import Network from "../../network";
 
 describe("solve", () => {
-    it("solves a network with only one solution", () => {
+    it("solves a network with only one solution", async () => {
         const network = Network.from([
             [1, 0, 0, 0],
             [0, 1, 0, 1],
             [1, 1, 0, 1],
             [1, 0, 1, 0],
         ]);
-        const { solution, hasMultipleSolutions } = network.solve();
+        const { solution, hasMultipleSolutions } = await network.solve();
         expect(solution).toContainEqual(expect.objectContaining({ rowId: 1 }));
         expect(solution).toContainEqual(expect.objectContaining({ rowId: 3 }));
         expect(hasMultipleSolutions).toBe(false);
     });
 
-    it("solves a network with multiple solutions", () => {
+    it("solves a network with multiple solutions", async () => {
         const network = Network.from([
             [1, 0, 0, 1],
             [0, 1, 0, 1],
             [0, 1, 0, 1],
             [1, 0, 1, 0],
         ]);
-        const { solution, hasMultipleSolutions } = network.solve();
+        const { solution, hasMultipleSolutions } = await network.solve();
         expect(solution).toContainEqual(expect.objectContaining({ rowId: 1 }));
         expect(solution).toContainEqual(expect.objectContaining({ rowId: 3 }));
         expect(hasMultipleSolutions).toBe(true);
