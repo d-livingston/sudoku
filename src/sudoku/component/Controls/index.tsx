519b194310dce13bb51626b646989fe5ed69d627
Daniel Livingston <me@daniellivingston.net>
2021-05-21 15:02:41 -0700

    Added functionality to controls

--- src/sudoku/component/Controls/index.tsx
+++ src/sudoku/component/Controls/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { Value } from "../Cell";
-import { SudokuReducerAction } from "../reducer";
+import {
+    deleteCell,
+    fillCell,
+    toggleNotes,
+    SudokuReducerAction,
+} from "../reducer";
 import styles from "./Controls.module.scss";
 
 export type ControlsProps = {
@@ -8,19 +13,36 @@ export type ControlsProps = {
     size: number;
 };
 
-export default function Controls({ size }: ControlsProps): JSX.Element {
+export default function Controls({
+    dispatch,
+    size,
+}: ControlsProps): JSX.Element {
     return (
         <div className={styles.container}>
             <div className={styles.number_pad}>
                 {Array.from({ length: size }, (_, i) => (
-                    <button className={styles.button} key={i}>
+                    <button
+                        className={styles.button}
+                        key={i}
+                        onClick={() => dispatch(fillCell(i + 1))}
+                    >
                         <Value value={i + 1} />
                     </button>
                 ))}
             </div>
             <div className={styles.other}>
-                <button className={styles.button}>Notes</button>
-                <button className={styles.button}>Delete</button>
+                <button
+                    className={styles.button}
+                    onClick={() => dispatch(toggleNotes())}
+                >
+                    Notes
+                </button>
+                <button
+                    className={styles.button}
+                    onClick={() => dispatch(deleteCell())}
+                >
+                    Delete
+                </button>
             </div>
         </div>
     );
