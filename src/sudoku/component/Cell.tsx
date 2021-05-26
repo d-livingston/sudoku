a6c9bcb43573962608456cb52f8fa8a3dc04df2c
Daniel Livingston <me@daniellivingston.net>
2021-05-25 17:43:23 -0700

    Fixed typo

--- src/sudoku/component/Cell.tsx
+++ src/sudoku/component/Cell.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import classNames from "classnames";
-import { CellNotes } from "./reducer";
+import { CellNotes } from "./notes";
 import styles from "./Cell.module.scss";
 
 export type CellProps = {
