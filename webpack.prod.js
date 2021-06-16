bfcfcd68eeec1e42f2dda4f6b4369e3e534418c9
Daniel Livingston <me@daniellivingston.net>
2021-06-15 19:02:09 -0700

    Updated to include source map in production for now

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require("webpack-merge");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
 const TerserPlugin = require("terser-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
+    devtool: "inline-source-map",
+    plugins: [new BundleAnalyzerPlugin()],
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
